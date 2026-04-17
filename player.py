54fc894669fa2bfd31ca5cb00cec18eb1443b6cd
nhanlenhanle <nhanks123tr@gmail.com>
2026-04-17 22:37:32 +0700

    sap full roi

--- player.py
+++ player.py
@@ -29,7 +29,7 @@ class Player:
         #------------------------- BOT ------------------------
         self.is_bot = False
         self._reset_debug_info()
-
+        self.cache_target_clear=pygame.Vector2(0,0)
     def _reset_debug_info(self):
         self.debug_info = {
             "state": "IDLE",
@@ -87,11 +87,8 @@ class Player:
 
             if dist < min_dist and dist != 0:
                 normal = diff.normalize()
-
-                # đẩy ra ngoài
                 pos = post + normal * min_dist
 
-                # phản xạ chuẩn
                 vel = vel - 2 * vel.dot(normal) * normal
                 vel *= 0.9
 
@@ -187,90 +184,94 @@ class Player:
             
         dir_to_ball = goal_to_ball_vec.normalize()
         ball_control_dist = 0 # (ball.pos - opponent.pos).length()
-        
-        # 1. KHI NÀO PHẢI LAO LÊN TẮC BÓNG (COMMIT)?
-        # - Địch để bóng dài (mất kiểm soát)
-        # - Hoặc bóng đã vào vùng nguy hiểm (cách gôn < 180 pixel)
+
         if ball_control_dist > PLAYER_RADIUS + BALL_RADIUS + 30 or dist_to_goal < 180:
             return ball.pos  # Lao thẳng vào bóng
 
-        # 2. THU HẸP GÓC ĐỘNG (DYNAMIC JOCKEYING)
-        # Ở xa thì lùi nhiều (max 100), càng gần gôn thì khoảng cách lùi càng giảm dần
         dynamic_buffer = min(100, dist_to_goal * 0.3) 
         
         target = opponent.pos - dir_to_ball * dynamic_buffer
 
         return target
-    @staticmethod
-    def ball_fly_goal(ball, dt, goal_x=FIELD_WIDTH):
-        max_t = 3.0
-        sim_dt = max(1 / 180, min(dt, 1 / 60))
-        pos = ball.pos.copy()
-        vel = ball.vel.copy()
-        time = 0.0
-        last_pos = pos.copy()
-        last_vel = vel.copy()
+    # @staticmethod
+    # def ball_fly_goal(ball, dt, goal_x=FIELD_WIDTH):
+    #     max_t = 3.0
+    #     sim_dt = max(1 / 180, min(dt, 1 / 60))
+    #     pos = ball.pos.copy()
+    #     vel = ball.vel.copy()
+    #     time = 0.0
+    #     last_pos = pos.copy()
+    #     last_vel = vel.copy()
 
-        while time < max_t:
-            last_pos = pos.copy()
-            last_vel = vel.copy()
-            pos, vel = Player.simulate_ball_step(pos, vel, sim_dt)
-            time += sim_dt
+    #     while time < max_t:
+    #         last_pos = pos.copy()
+    #         last_vel = vel.copy()
+    #         pos, vel = Player.simulate_ball_step(pos, vel, sim_dt)
+    #         time += sim_dt
 
-            if goal_x >= FIELD_WIDTH:
-                if GOAL_TOP < pos.y < GOAL_BOTTOM and pos.x - BALL_RADIUS / 3 > FIELD_WIDTH:
-                    return {
-                        "time": time,
-                        "pos": pos.copy(),
-                        "vel": vel.copy(),
-                        "last_pos": last_pos,
-                        "last_vel": last_vel,
-                    }
-            else:
-                if GOAL_TOP < pos.y < GOAL_BOTTOM and pos.x + BALL_RADIUS / 3 < 0:
-                    return {
-                        "time": time,
-                        "pos": pos.copy(),
-                        "vel": vel.copy(),
-                        "last_pos": last_pos,
-                        "last_vel": last_vel,
-                    }
+    #         if goal_x >= FIELD_WIDTH:
+    #             if GOAL_TOP < pos.y < GOAL_BOTTOM and pos.x - BALL_RADIUS / 3 > FIELD_WIDTH:
+    #                 return {
+    #                     "time": time,
+    #                     "pos": pos.copy(),
+    #                     "vel": vel.copy(),
+    #                     "last_pos": last_pos,
+    #                     "last_vel": last_vel,
+    #                 }
+    #         else:
+    #             if GOAL_TOP < pos.y < GOAL_BOTTOM and pos.x + BALL_RADIUS / 3 < 0:
+    #                 return {
+    #                     "time": time,
+    #                     "pos": pos.copy(),
+    #                     "vel": vel.copy(),
+    #                     "last_pos": last_pos,
+    #                     "last_vel": last_vel,
+    #                 }
 
-            if vel.length_squared() < 25:
-                break
+    #         if vel.length_squared() < 25:
+    #             break
 
-        return None
+    #     return None
 
-    @staticmethod
-    def should_bank_save(player, intercept_pos, intercept_vel, my_goal):
-        goal_vec = my_goal - intercept_pos
-        if goal_vec.length_squared() <= 1e-6:
-            return False
+    # @staticmethod
+    # def should_bank_save(player, intercept_pos, intercept_vel, my_goal):
+    #     goal_vec = my_goal - intercept_pos
+    #     if goal_vec.length_squared() <= 1e-6:
+    #         return False
 
-        goal_dir = goal_vec.normalize()
-        bot_run_vec = intercept_pos - player.pos
+    #     goal_dir = goal_vec.normalize()
+    #     bot_run_vec = intercept_pos - player.pos
 
-        bot_dot = 0.0
-        if bot_run_vec.length_squared() > 1e-6:
-            bot_dot = bot_run_vec.normalize().dot(goal_dir)
+    #     bot_dot = 0.0
+    #     if bot_run_vec.length_squared() > 1e-6:
+    #         bot_dot = bot_run_vec.normalize().dot(goal_dir)
 
-        ball_dot = 0.0
-        if intercept_vel.length_squared() > 1e-6:
-            ball_dot = intercept_vel.normalize().dot(goal_dir)
+    #     ball_dot = 0.0
+    #     if intercept_vel.length_squared() > 1e-6:
+    #         ball_dot = intercept_vel.normalize().dot(goal_dir)
 
-        return bot_dot > 0.15 and ball_dot > 0.15
+    #     return bot_dot > 0.15 and ball_dot > 0.15
 
-    @staticmethod
-    def goal_bank_target(intercept_pos):
-        inset_x = FIELD_WIDTH - 6
-        inset_y = 10
-        top_target = pygame.Vector2(inset_x, GOAL_TOP - inset_y)
-        bottom_target = pygame.Vector2(inset_x, GOAL_BOTTOM + inset_y)
+    # @staticmethod
+    # def goal_bank_target(intercept_pos):
+    #     inset_x = FIELD_WIDTH
+    #     inset_y = 10
+    #     top_target = pygame.Vector2(inset_x, GOAL_TOP - inset_y)
+    #     bottom_target = pygame.Vector2(inset_x, GOAL_BOTTOM + inset_y)
 
-        if abs(intercept_pos.y - top_target.y) <= abs(intercept_pos.y - bottom_target.y):
-            return top_target
-        return bottom_target
-
+    #     if abs(intercept_pos.y - top_target.y) <= abs(intercept_pos.y - bottom_target.y):
+    #         return top_target
+    #     return bottom_target
+    def find_best_clear(self,ball,bot_intercept):
+        dir = self.pos - ball.pos
+        target_top = pygame.Vector2(FIELD_WIDTH-5,5)
+        target_bottom = pygame.Vector2(FIELD_WIDTH-5,FIELD_HEIGHT-5)
+        self.cache_target_clear=pygame.Vector2(0,0)
+        B = (target_top - bot_intercept)
+        C = (target_bottom - bot_intercept)
+        if (B.length()<=C.length()):
+            return target_top
+        return target_bottom
     @staticmethod
     def is_player_ball_line_on_goal(self, ball):
         A = self.pos
@@ -288,8 +289,27 @@ class Player:
         if t <= 0:
             return False
         y_hit = A.y + final_vel.y * t
-        Radius=POST_RADIUS + BALL_RADIUS + 0.01
+        Radius=POST_RADIUS + BALL_RADIUS + 1
         if GOAL_TOP + Radius <= y_hit <= GOAL_BOTTOM - Radius:
+            return True
+    def is_not_player_ball_line_on_goal(self, ball):
+        A = self.pos
+        dir = ball.pos - self.pos
+        if dir.length_squared() < 1e-6:
+            return False
+        shot_dir = (ball.pos - self.pos).normalize()
+        final_vel = ball.vel + shot_dir * KICK_FORCE
+        goal_x = FIELD_WIDTH
+        if final_vel.x <= 0:
+            return False
+        if abs(final_vel.x) < 1e-6:
+            return False
+        t = (goal_x - A.x) / final_vel.x
+        if t <= 0:
+            return False
+        y_hit = A.y + final_vel.y * t
+        Radius=POST_RADIUS + BALL_RADIUS + 1
+        if GOAL_TOP - Radius > y_hit or  y_hit > GOAL_BOTTOM + Radius:
             return True
     def find_best_goal_target(ball_pos, goal_center, goal_top, goal_bottom):
         
@@ -325,6 +345,10 @@ class Player:
         if (candidate_a - player_pos).length_squared() <= (candidate_b - player_pos).length_squared():
             return candidate_a
         return candidate_b
+    def NEED_SPAM(self, opponent, ball):
+        if (ball.pos-self.pos).normalize().dot((ball.pos-opponent.pos).normalize())<0 and (ball.pos-self.pos).normalize().x<0:
+           return((self.pos-opponent.pos).length() - (PLAYER_RADIUS*2 + BALL_RADIUS + 150) <= 0)
+        return False
     def bot_update(self, opponent, ball, dt):
         enemy_goal_center = pygame.Vector2(0, FIELD_HEIGHT // 2)
         my_goal = pygame.Vector2(FIELD_WIDTH, FIELD_HEIGHT // 2)
@@ -342,51 +366,84 @@ class Player:
         is_pressed = False
         planned_shot_direction = pygame.Vector2(0, 0)
         path_clear = None
-        goal_threat = Player.ball_fly_goal(ball, dt, my_goal.x)
+        #goal_threat = Player.ball_fly_goal(ball, dt, my_goal.x)
         diff = ball.pos - self.pos
-        if goal_threat or (t_bot - t_enemy < 0.15 and t_bot - t_enemy > -0.15):
-            save_intercept = bot_intercept
-            save_intercept_vel = bot_intercept_info["vel"]
+        # if goal_threat or (t_bot - t_enemy < 0.15 and t_bot - t_enemy > -0.15 and diff.normolize()>0) :
+        #     save_intercept = bot_intercept
+        #     save_intercept_vel = bot_intercept_info["vel"]
 
-            if my_goal.x >= FIELD_WIDTH and save_intercept.x > FIELD_WIDTH - BALL_RADIUS:
-                save_intercept = goal_threat["last_pos"].copy()
-                save_intercept_vel = goal_threat["last_vel"].copy()
-            elif my_goal.x <= 0 and save_intercept.x < BALL_RADIUS:
-                save_intercept = goal_threat["last_pos"].copy()
-                save_intercept_vel = goal_threat["last_vel"].copy()
+        #     if my_goal.x >= FIELD_WIDTH and save_intercept.x > FIELD_WIDTH - BALL_RADIUS:
+        #         save_intercept = goal_threat["last_pos"].copy()
+        #         save_intercept_vel = goal_threat["last_vel"].copy()
+        #     elif my_goal.x <= 0 and save_intercept.x < BALL_RADIUS:
+        #         save_intercept = goal_threat["last_pos"].copy()
+        #         save_intercept_vel = goal_threat["last_vel"].copy()
 
-            bot_intercept = save_intercept
-            t_bot = Player.estimate_time(self, bot_intercept)
-            if Player.should_bank_save(self, save_intercept, save_intercept_vel, my_goal):
-                state = "SAVE_WALL"
-                bank_target = Player.goal_bank_target(save_intercept)
-                planned_shot = bank_target - save_intercept
+        #     bot_intercept = save_intercept
+        #     t_bot = Player.estimate_time(self, bot_intercept)
+        #     if Player.should_bank_save(self, save_intercept, save_intercept_vel, my_goal):
+        #         state = "SAVE_WALL"
+        #         bank_target = Player.goal_bank_target(save_intercept)
+        #         planned_shot = bank_target - save_intercept
 
-                if planned_shot.length_squared() > 1e-6:
-                    planned_shot_direction = planned_shot.normalize()
-                    target = save_intercept - planned_shot_direction * (PLAYER_RADIUS + BALL_RADIUS + 0.5)
-                else:
-                    target = save_intercept
+        #         if planned_shot.length_squared() > 1e-6:
+        #             planned_shot_direction = planned_shot.normalize()
+        #             target = save_intercept - planned_shot_direction * (PLAYER_RADIUS + BALL_RADIUS + 0.5)
+        #         else:
+        #             target = save_intercept
 
-                if (self.pos - target).length() < 14:
-                    should_kick = True
-            else:
-                state = "SAVE_CLEAR"
-                target = save_intercept
-                clear_vec = save_intercept - my_goal
-                if clear_vec.length_squared() > 1e-6:
-                    planned_shot_direction = clear_vec.normalize()
-                should_kick = True
+        #         if (self.pos - target).length() < 14:
+        #             should_kick = True
+        #     else:
+        #         state = "SAVE_CLEAR"
+        #         target = save_intercept
+        #         clear_vec = save_intercept - my_goal
+        #         if clear_vec.length_squared() > 1e-6:
+        #             planned_shot_direction = clear_vec.normalize()
+        #         should_kick = True
+        if (ball.pos-self.pos).normalize().dot((ball.pos-opponent.pos).normalize())<0 and (ball.pos-self.pos).normalize().x<0:
+            ok = 0.3
+        else:
+            ok = -0.15
+        check_ok = (self.pos-ball.pos).normalize().dot((self.pos-opponent.pos).normalize())<0 and (ball.pos-self.pos).normalize().x>0
+        check_ok = check_ok and (t_bot > t_enemy + 0.15)
+        if t_bot > t_enemy + ok and (not check_ok):
+            state = "DEFEND"
+        elif t_bot - t_enemy < 0.15 and t_bot - t_enemy > -1 and diff.normalize().x >=0:
+            state = "CLEAR"
+        elif self.NEED_SPAM(opponent,ball):
+            state = "SPAM"
         elif diff.length() < PLAYER_RADIUS + BALL_RADIUS + KICK_RANGE:
             state = "ATTACK"
-        elif t_bot > t_enemy + 0.15:
-            state = "DEFEND"
         else:
             state = "ATTACK"
         #nơi cái phần attack_vec tôi muốn thêm cái attack_real ý là vì ball dang có vel thì với attack_vec không thì nó ko chính xác, attack_vec nó được xem như cái mong muốn; thì tôi có công thức attack_vec-ball.vel=attack_vec_real có đúng ko
-        if state == "DEFEND":
+        if state == "CLEAR":
+            vec_excepted = self.find_best_clear(ball,bot_intercept) - ball.pos
+            # print(vec_excepted)
+            vec = vec_excepted - ball.vel
+            #vec = vec_excepted
+            if vec.length_squared() > 1e-6:
+                planned_shot_direction = vec.normalize()
+                bot_to_ball = ball.pos - self.pos
+                target = ball.pos - planned_shot_direction * (PLAYER_RADIUS + BALL_RADIUS+0.1)
+                if bot_to_ball.length_squared() > 1e-6:
+                    behind_alignment = bot_to_ball.normalize().dot(planned_shot_direction)
+                else:
+                    behind_alignment = 1.0
+                if behind_alignment > 0.98 or Player.is_not_player_ball_line_on_goal(self,ball):
+                    should_kick = True
+                else:
+                    should_kick = False
+                    target = Player.orbit_attack_target(self.pos, ball.pos, target)
+            else:
+                target = ball.pos
+                should_kick = False
+        elif state == "DEFEND":
+            self.cache_target_clear=pygame.Vector2(0,0)
             target = Player.jockey_position(self, ball, opponent)
         elif state == "ATTACK":
+            self.cache_target_clear=pygame.Vector2(0,0)
             # Chọn điểm đích tốt nhất từ 3 điểm: center, top, bottom
             goal_center = enemy_goal_center
             goal_top = pygame.Vector2(0, GOAL_TOP + 0.1)
@@ -395,6 +452,7 @@ class Player:
             best_goal_target = Player.find_best_goal_target(ball.pos, goal_center, goal_top, goal_bottom)
             attack_vec_excepted = best_goal_target - ball.pos
             attack_vec = attack_vec_excepted - ball.vel
+            check_it = self.is_not_player_ball_line_on_goal(ball) and ball.pos.x>=FIELD_WIDTH-200
             if attack_vec.length_squared() > 1e-6:
                 planned_shot_direction = attack_vec.normalize()
                 target = ball.pos - planned_shot_direction * (PLAYER_RADIUS + BALL_RADIUS+0.1)
@@ -404,7 +462,7 @@ class Player:
                     behind_alignment = bot_to_ball.normalize().dot(planned_shot_direction)
                 else:
                     behind_alignment = 1.0
-                if behind_alignment > 0.99 or Player.is_player_ball_line_on_goal(self,ball):
+                if behind_alignment > 0.99 or Player.is_player_ball_line_on_goal(self,ball) or check_it:
                     should_kick = True
                 else:
                     should_kick = False
@@ -412,7 +470,9 @@ class Player:
             else:
                 target = ball.pos
                 should_kick = False
-
+        elif state == "SPAM":
+            should_kick = True
+            target = ball.pos
         direction = target - self.pos
         hack_speed = 1.05
         if direction.length() > 0.5:
